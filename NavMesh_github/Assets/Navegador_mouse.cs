a7421a3bb57808a159222f31e3113240bef80cd0
Julio Arath Rosales <35709739+arath11@users.noreply.github.com>
2020-05-22 21:53:56 -0500

    NavMesh practice with mouse movement in Unity

---
+++ NavMesh_github/Assets/Navegador_mouse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navegador_mouse : MonoBehaviour
+{
+    public Camera cam;
+
+    public NavMeshAgent navmesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                navmesh.SetDestination(hit.point);
+            }
+
+        }
+
+    }
+}
